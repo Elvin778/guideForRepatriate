c73a785f2121491ddc55fa65778d0009cc7834c2
Alexander Tumanov <alvtum@gmail.com>
2017-02-27 20:50:27 +0200

    Initial Commit

---
+++ NavDrawMore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NavDrawMore
+//
+//  Created by Александр Туманов on 2/27/17.
+//  Copyright © 2017 Александр Туманов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
